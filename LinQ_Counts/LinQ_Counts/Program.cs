2505c75454e3f25777699f2bcaec8ce10ec35ccd
MichalLegen <legen.michal@outlook.com>
2015-11-29 18:38:36 +0100

    Interesting LinQ_Count usage

---
+++ LinQ_Counts/LinQ_Counts/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        var n = Double.Parse(Console.ReadLine());
+        var a = Console.ReadLine().Split(' ').Select(val => Int32.Parse(val));
+
+        Console.WriteLine(a.Count(val => val > 0) / n);
+        Console.WriteLine(a.Count(val => val < 0) / n);
+        Console.WriteLine(a.Count(val => val == 0) / n);
+        Console.Read();
+    }
+}
